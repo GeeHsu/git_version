40ca7b86212fda3520ebd46b83b2e5633c800224
GeeHsu <james81429@yahoo.com.tw>
2024-03-14 17:03:44 +0800

    如何在 Cli 環境中加入 Vue 元件

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # git_version
-練習 Git 版本控制
+練習 Git 版本控制 ch6 - vue_cli
---
+++ vue_cli/new-project/src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <!-- 7-4. Card 呈現在畫面上 -->
+    <Card></Card>
+  </div>
+</template>
+
+<script>
+// 7-2. 外部元件匯入進來
+import Card from '@/components/Card.vue';
+
+// 7-1. 元件 js 匯出
+export default {
+  // 7-3. 區域註冊
+  components: {
+    Card,
+  },
+};
+</script>
