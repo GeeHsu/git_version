52d83a233d67234aad86a8e2c22362e9357e19be
GeeHsu <james81429@yahoo.com.tw>
2024-03-19 11:24:16 +0800

    中場: 為目前專案加入樣式

--- vue_router/vue_router_record/src/App.vue
+++ vue_router/vue_router_record/src/App.vue
@@ -1,11 +1,30 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <!-- 動態屬性 -->
-    <router-link :to="{ name: '新增頁面' }">新增頁面</router-link>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button"
+        data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link active" aria-current="page">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: '新增頁面' }" class="nav-link">新增頁面</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
